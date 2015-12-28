18ba4f885c10e7a1852e80c16cf75e99318f4009
kukks <kukks@Home-Server>
2015-12-28 21:33:14 +0700

    28/12/2558

--- sql/calc_depression_older.sql
+++ sql/calc_depression_older.sql
@@ -17,6 +17,7 @@ and v.vstdate between '2015-10-01' and '2016-09-30'
 and p.death<>'y'
 and p.patient_hn=v.hn and p.hospcode=v.hospcode) as ds;
 
+
 DROP TABLE IF EXISTS ltc_depression_summary ;
 CREATE TABLE ltc_depression_summary select * from (select b.*,a.Amphur_name as ampname  from (select hc.hospcode,
 concat(h.hosptype,h.`name`) as hospname,
@@ -42,72 +43,73 @@ and p.age_y>59
 group by p.hospcode) AS po ON po.hospcode= hc.hospcode
 
 left join (select 
-count(distinct d.person_id) as screen ,
+count(distinct dp1.person_id) as screen ,
 d.hospcode,d.last_calc 
 from (select
-max(vn),hospcode,person_id,feel_boring_2_week,feel_depression_2_week,depression_score,dp.last_calc
+max(vn) as last_vn,hospcode,person_id,feel_boring_2_week,feel_depression_2_week,depression_score,dp.last_calc
 from
 ltc_depression_screen AS dp where dp.house_regist_type_id in (1,3)
 group by dp.hospcode,dp.person_id) as d
+join ltc_depression_screen dp1 on dp1.vn=d.last_vn 
 group by d.hospcode) as d1 on d1.hospcode=hc.hospcode
 
 left join (select
 count(distinct dp.person_id) as non_risk ,
 dp.hospcode,dp.last_calc
-from
-(select
-max(vn),hospcode,person_id,feel_boring_2_week,feel_depression_2_week,depression_score,dp.last_calc
+from (select
+max(vn) as last_vn
 from
 ltc_depression_screen AS dp where dp.house_regist_type_id in (1,3)
-group by dp.hospcode,dp.person_id) AS dp
-where dp.feel_boring_2_week='N' and feel_depression_2_week='N' and dp.depression_score<7
+group by dp.hospcode,dp.person_id) as d
+join ltc_depression_screen dp on dp.vn=d.last_vn 
+where dp.feel_boring_2_week='N' and dp.feel_depression_2_week='N' and dp.depression_score<7
 group by dp.hospcode) as d0 on d0.hospcode=hc.hospcode
 
 left join (select
 count(distinct dp.person_id) as risk ,
 dp.hospcode,dp.last_calc
-from
-(select
-max(vn),hospcode,person_id,feel_boring_2_week,feel_depression_2_week,depression_score,dp.last_calc
+from (select
+max(vn) as last_vn
 from
 ltc_depression_screen AS dp where dp.house_regist_type_id in (1,3)
-group by dp.hospcode,dp.person_id) AS dp
+group by dp.hospcode,dp.person_id) as d
+join ltc_depression_screen dp on dp.vn=d.last_vn 
 where (dp.feel_boring_2_week='Y' or feel_depression_2_week='Y') and dp.depression_score>6 
 group by dp.hospcode) as d2 on d2.hospcode=hc.hospcode
 
 left join (select
 count(distinct dp.person_id) as mild ,
 dp.hospcode,dp.last_calc
-from
-(select
-max(vn),hospcode,person_id,feel_boring_2_week,feel_depression_2_week,depression_score,dp.last_calc
+from (select
+max(vn) as last_vn
 from
 ltc_depression_screen AS dp where dp.house_regist_type_id in (1,3)
-group by dp.hospcode,dp.person_id) AS dp
+group by dp.hospcode,dp.person_id) as d
+join ltc_depression_screen dp on dp.vn=d.last_vn 
 where (dp.feel_boring_2_week='Y' or feel_depression_2_week='Y') and dp.depression_score between '7' and '12'
 group by dp.hospcode) as d3 on d3.hospcode=hc.hospcode
 
 left join (select
 count(distinct dp.person_id) as moder ,
 dp.hospcode,dp.last_calc
-from
-(select
-max(vn),hospcode,person_id,feel_boring_2_week,feel_depression_2_week,depression_score,dp.last_calc
+from (select
+max(vn) as last_vn
 from
 ltc_depression_screen AS dp where dp.house_regist_type_id in (1,3)
-group by dp.hospcode,dp.person_id) AS dp
+group by dp.hospcode,dp.person_id) as d
+join ltc_depression_screen dp on dp.vn=d.last_vn 
 where (dp.feel_boring_2_week='Y' or feel_depression_2_week='Y') and dp.depression_score between '13' and '18'
 group by dp.hospcode) as d4 on d4.hospcode=hc.hospcode
 
 left join (select
 count(distinct dp.person_id) as seve ,
 dp.hospcode,dp.last_calc
-from
-(select
-max(vn),hospcode,person_id,feel_boring_2_week,feel_depression_2_week,depression_score,dp.last_calc
+from (select
+max(vn) as last_vn
 from
 ltc_depression_screen AS dp where dp.house_regist_type_id in (1,3)
-group by dp.hospcode,dp.person_id) AS dp
+group by dp.hospcode,dp.person_id) as d
+join ltc_depression_screen dp on dp.vn=d.last_vn 
 where (dp.feel_boring_2_week='Y' or feel_depression_2_week='Y') and dp.depression_score >18
 group by dp.hospcode) as d5 on d5.hospcode=hc.hospcode) as b
 join Amphur a on b.amp=a.Amphur ) as d1; 
