5ca11aaf0d986daeb36014a47cad01df6e6c2b6b
admin <admin@PC-Server>
2016-01-09 19:43:41 +0700

    2015-01-06 ->add highcharts-ng

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -149,6 +149,7 @@ myApp.controller("homeController", function ($scope, $http) {
                 var data3 = [];
                 var data4 = [];
                 for (var i = 0; i < $scope.chartdata.length; i++) {
+
                     cat.push($scope.chartdata[i]['name']);
                     data1.push(parseInt($scope.chartdata[i]['p1']));
                     data2.push(parseInt($scope.chartdata[i]['p2']));
@@ -176,14 +177,14 @@ myApp.controller("homeController", function ($scope, $http) {
                         tooltip: {
                             headerFormat: '<span style="font-size:14px">อำเภอ{point.key}</span><table>',
                             pointFormat: '<tr><td style="color:{series.color};padding:0"><b>{series.name}:</b> </td>' +
-                                    '<td style="padding:0"><b>{point.y:,.0f} คน</b></td></tr>',
+                                    '<td style="padding:0"><b>{point.percentage:,.2f} %</b></td></tr>',
                             footerFormat: '</table>',
                             shared: true,
                             useHTML: true
                         },
                         plotOptions: {
                             column: {
-                                //stacking: 'percent',
+                                stacking: 'percent',
                                 pointPadding: 0.05,
                                 borderWidth: 0
                             }
@@ -210,7 +211,7 @@ myApp.controller("homeController", function ($scope, $http) {
                             //colorByPoint: true,
                             //color: '#FA58F4',
                             data: data3
-                        },{
+                        }, {
                             name: '90ปีขึ้นไป',
                             //colorByPoint: true,
                             //color: '#FA58F4',
@@ -264,7 +265,7 @@ myApp.controller("olderController", function ($scope, $http) {
                     data3.push(parseInt($scope.older[i]['t3']));
                 }
                 //d.push(data)
-                
+
                 //start charts   
                 $scope.olderADLChart = {
                     options: {
@@ -324,8 +325,8 @@ myApp.controller("olderController", function ($scope, $http) {
 
                 }
                 //end charts                   
-                
-                
+
+
 
 
             })
@@ -383,32 +384,114 @@ myApp.controller("gastricController", function ($scope, $http) {
 
 
 
-/*myApp.controller("olderDController", function ($stateParams,$scope,$http) {
- $scope.dataloaded = false;
- $scope.cupcode=$stateParams.cupcode;
- $http.get('dataService/m_pop_older_d.php?cupcode='+$scope.cupcode)
- .success(function (response) {
- $scope.older = response.records;
- $scope.dataloaded = true;
- })
- .error(function () {
- alert(ไม่สามารถประมวลผลข้อมูลได้);
- 
- });
- 
- })*/
 
-/*myApp.controller('olderDController', ['$scope','$stateParams','$http', function($stateParams,$scope,$http) {
- $scope.dataloaded = false;
- $scope.cupcode=$stateParams.cupcode;
- $http.get('dataService/m_pop_older_d.php?cupcode='+$scope.cupcode)
- .success(function (response) {
- $scope.older = response.records;
- $scope.dataloaded = true;
- })
- .error(function () {
- alert(ไม่สามารถประมวลผลข้อมูลได้);
- 
- });
- }])*/
+TREND_LIST = [
+    {drilldown: "Normal",
+        name: "ปกติ",
+        visible: true,
+        y: 12},
+    {drilldown: "Trend2",
+        name: "Trend2",
+        visible: true,
+        y: 13},
+    {drilldown: "Trend3",
+        name: "Trend3",
+        visible: true,
+        y: 25},
+    {drilldown: "Trend4",
+        name: "Trend4",
+        visible: true,
+        y: 50}
+]
+
+NUMBER_OF_OFFERS_BY_TREND = [
+    {id: "Normal",
+        name: "ปกติ",
+        data: [["อำเภอ ก", 50], ["อำเภอ ข", 30], ["อำเภอ ค", 20]]
+    },
+    {id: "Trend2", name: "Trend2",
+        data: [["Offer3", 20], ["Offer4", 10], ["Offer5", 40], ["Offer6", 30]]
+    },
+    {id: "Trend3", name: "Trend3",
+        data: [["Offer7", 70], ["Offer8", 30]]},
+    {id: "Trend4", name: "Trend4",
+        data: [["Offer9", 15], ["Offer10", 35], ["Offer11", 20], ["Offer12", 30]]}
+]
+
+
+
+
+myApp.controller("MyController", function ($scope) {
+    var chartConfig = {
+        title: {
+            text: 'Number of offers by trend'
+        },
+        subtitle: {
+            text: 'My company'
+        },
+        plotOptions: {
+            series: {
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.name}: {point.y:.1f}%'
+                }
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+            pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+        },
+        options: {
+            chart: {
+                type: 'column'
+            },
+            drilldown: {
+                series: NUMBER_OF_OFFERS_BY_TREND
+            },
+            xAxis: {
+                type: 'category'
+            },
+            yAxis: {
+                title: {
+                    text: 'Total percent market share'
+                }
+
+            },
+            legend: {
+                align: 'right',
+                x: -70,
+                verticalAlign: 'top',
+                y: 20,
+                floating: true,
+                backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+                borderColor: '#CCC',
+                borderWidth: 1,
+                shadow: false
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+                pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+            }
+            ,
+            plotOptions: {
+                column: {
+                    stacking: 'normal',
+                    dataLabels: {
+                        enabled: true,
+                        color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                        style: {
+                            textShadow: '0 0 0px black, 0 0 0px black'
+                        }
+                    }
+                }
+            }
+        },
+        series: [{
+                name: 'Trends',
+                colorByPoint: true,
+                data: TREND_LIST
+            }]
+    };
+    $scope.chartConfig = chartConfig;
+})
 
